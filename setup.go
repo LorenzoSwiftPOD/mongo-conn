d3e8a5f50177ea3ebbbd4a18d16f734803bb308a
Carlos Lorenzo Lopez <carlos@swiftpod.com>
2023-09-11 19:47:49 -0700

    mode added

--- setup.go
+++ setup.go
@@ -4,6 +4,8 @@ import (
 	"os"
 )
 
+var Mode string = "production"
+
 type tokens struct {
 	uri           string
 	user          string
@@ -23,5 +25,13 @@ func (t *tokens) init() {
 	t.password = os.Getenv("MongoPassword")
 	t.authDatabase = os.Getenv("MongoAuthDatabase")
 	t.authMechanism = os.Getenv("MongoAuthMechanism")
+	if Mode == "local" {
+		t.uri = os.Getenv("LocalMongoURI")
+		t.port = os.Getenv("LocalMongoPort")
+		t.user = os.Getenv("LocalMongoUser")
+		t.password = os.Getenv("LocalMongoPassword")
+		t.authDatabase = os.Getenv("LocalMongoAuthDatabase")
+		t.authMechanism = os.Getenv("LocalMongoAuthMechanism")
+	}
 
 }
